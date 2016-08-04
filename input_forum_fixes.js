96b30390f1d44f600d1dcfc3d302f37856ced365
root <root@vultr.guest>
2016-08-04 15:38:39 -0300

    a

--- input_forum_fixes.js
+++
@@ -1,7 +0,0 @@
-$(document).ready(function(){
-	$(".discussion-show.control-button").trigger("click");
-	$("input:text").keypress(function() {
-		this.value = (this.value).replace(/`/g , "'");
-		this.value = (this.value).replace(/´/g , "'");
-	});
-});
